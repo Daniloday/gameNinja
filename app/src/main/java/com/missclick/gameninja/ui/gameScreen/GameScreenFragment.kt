09578ac39ec0dc2794ab0c709e228ddcd3d3bd2b
daniloday <danil.vladyka1@gmail.com>
2021-02-09 23:58:05 +0200

    logic

--- app/src/main/java/com/missclick/gameninja/ui/gameScreen/GameScreenFragment.kt
+++ app/src/main/java/com/missclick/gameninja/ui/gameScreen/GameScreenFragment.kt
@@ -1,6 +1,8 @@
 package com.missclick.gameninja.ui.gameScreen
 
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,7 +11,10 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.findNavController
 import com.missclick.gameninja.R
+import java.util.concurrent.TimeUnit
+
 
 class GameScreenFragment : Fragment() {
 
@@ -33,7 +38,7 @@ class GameScreenFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val positions = mutableListOf<ImageView>()
+        val positionsImage = mutableListOf<ImageView>()
         val pos0 : ImageView = view.findViewById(R.id.pos1_btn)
         val pos1 : ImageView = view.findViewById(R.id.pos2_btn)
         val pos2 : ImageView = view.findViewById(R.id.pos3_btn)
@@ -47,20 +52,65 @@ class GameScreenFragment : Fragment() {
         val pos10 : ImageView = view.findViewById(R.id.pos11_btn)
         val pos11 : ImageView = view.findViewById(R.id.pos12_btn)
 
-        positions.add(pos0)
-        positions.add(pos1)
-        positions.add(pos2)
-        positions.add(pos3)
-        positions.add(pos4)
-        positions.add(pos5)
-        positions.add(pos6)
-        positions.add(pos7)
-        positions.add(pos8)
-        positions.add(pos9)
-        positions.add(pos10)
-        positions.add(pos11)
+        positionsImage.add(pos0)
+        positionsImage.add(pos1)
+        positionsImage.add(pos2)
+        positionsImage.add(pos3)
+        positionsImage.add(pos4)
+        positionsImage.add(pos5)
+        positionsImage.add(pos6)
+        positionsImage.add(pos7)
+        positionsImage.add(pos8)
+        positionsImage.add(pos9)
+        positionsImage.add(pos10)
+        positionsImage.add(pos11)
 
-        val score = 0
+        var positions = listOf(1,1,2,2,3,3,4,4,5,5,6,6)
+        positions = positions.shuffled()
+        Log.e("List",positions.toString())
+        var isOpened = -1
+        var fails = 0
+        var pairs = 6
+        for (i in 0..11){
+            positionsImage[i].setOnClickListener {
+                if (isOpened == -1 || isOpened == i){
+                    isOpened = i
+                    positionsImage[i].setImageResource(getResource(number = positions[i]))
+                }
+                else{
+                    positionsImage[i].setImageResource(getResource(number = positions[i]))
+                    Handler().postDelayed(
+                            {
+                                if (positions[isOpened] != positions[i]){
+                                    fails += 1
+                                    Log.e(isOpened.toString(),i.toString())
+                                    positionsImage[i].setImageResource(getResource(number = 0))
+                                    positionsImage[isOpened].setImageResource(getResource(number = 0))
+                                    isOpened = -1
+                                }
+                                else{
+                                    positionsImage[i].visibility = View.INVISIBLE
+                                    positionsImage[isOpened].visibility = View.INVISIBLE
+                                    isOpened = -1
+                                    pairs -= 1
+                                    if (pairs == 0) findNavController().navigate(R.id.nav_result_screen)
+                                }
+                            },
+                            1000 // value in milliseconds
+                    )
 
+                }
+            }
+        }
+    }
+
+    private fun getResource(number : Int): Int {
+        if (number == 1) return R.drawable.one
+        if (number == 2) return R.drawable.two
+        if (number == 3) return R.drawable.third
+        if (number == 4) return R.drawable.four
+        if (number == 5) return R.drawable.fifth
+        if (number == 6) return R.drawable.six
+        return  R.drawable.back
     }
 }
